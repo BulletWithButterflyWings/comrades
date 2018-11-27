313ba3fc9d3d720edff6491ad946d6a51b8bd208
Alfons Örtemyr <oral16@nti.umea>
2018-11-27 10:54:26 +0100

    Del 5 (Rensa logg)

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
-choice = 0 
-message = ""
+choice = 0 # Ger "choice" värdet 0.
 
 #import datetime
 #print (datetime.date())
 
-while choice != 3:
+while choice != 4: # Loopar allt understående till man trycker avsluta (3).
 
-    print("***Välkommen till loggboken***\n")
-    print("1. Läs loggar ")
-    print("2. Skriv en logg")
-    print("3. Avsluta")
+    print("***Välkommen till loggboken***\n") # Skriver ut menyn i terminalen.
+    print("1. Läs loggar ") 
+    print("2. Skriv en logg") 
+    print("3. Rensa logg")
+    print("4. Avsluta") 
 
-    try: 
-        choice = int(input("\nGör ett val: "))
-    except:
-        print("Du gjorde inte ett korrekt val.")
+    try: # Felsöker programet så att man undviker kraschar i koden.
+        choice = int(input("\nGör ett val: ")) # Sparar det du skriver in i variabeln "choice". 
+    except: # Ifall du skriver in en bokstav, så säger den åt dig att skriva om.
+        print("Du gjorde inte ett korrekt val.") #
 
-    if choice == 1: 
-        f = open('log.txt' , 'r')
-        for line in f:
-                print(line)
-        f.close()          
-    elif choice == 2:
-        f = open('log.txt' , 'a+')
-        f.write(input("Skriv: ")) 
-        f.close()
-    else:
-        print("\nStänger av programet...")
+    if choice == 1: # Ifall du väljer alternativ 1 så skrivs understående ut.
+        f = open('log.txt' , 'r') # Öppnar upp filen "log.txt" med full rättighet för ändringar.
+        for line in f: # Den säger åt att printa varje linje i textloggen.
+                print(line) # Skriver ut raden från log.txt.
+        f.close()          # Stänger log.txt. 
+   
+    elif choice == 2: # Ifall du väljer alternativ 2 så sker understående. 
+        f = open('log.txt' , 'a+') # Du öppnar log.txt, med a+(append) som gör att det skrivs ut på nya rader varje gång.
+        f.write(input("Skriv: ")) # Du kan skriva i dokumentet (log.txt).
+        f.close() # Stänger textfil (log.txt).
+    
+    elif choice == 3:
+        f = open('log.txt' , 'w+')
+        f.write("")
+    else: # Om inte 1 eller 2 skrivs in så stängs programet.
+        print("\nStänger av programet...") 
