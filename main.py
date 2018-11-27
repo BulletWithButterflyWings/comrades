d1970fb553b29c9ba88ff701417a99db8c2d07eb
Alfons Örtemyr <oral16@nti.umea>
2018-11-27 09:07:09 +0100

    Del 2

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 choice = 0 
 message = ""
-import datetime
-print (datetime.date())
+meny = 0
+
+#import datetime
+#print (datetime.date())
+
+while meny != 3:
+
+    print("***Välkommen till loggboken***.\n")
+    print("1. Läs loggar ")
+    print("2. Skriv en logg")
+    print("3. Avsluta")
+
+    try: 
+        meny = int(input("Gör ett val: "))
+    except:
+        print("Du gjorde inte ett korrekt val.")
